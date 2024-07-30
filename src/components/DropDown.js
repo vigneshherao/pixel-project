e13772d68f378a1edfa97872a4bc172451899c89
vignesh <vigneshfornavy@gmail.com>
2024-07-30 23:36:39 +0530

    Implementation of Filter Buttons Component

---
+++ src/components/DropDown.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaAngleDown } from 'react-icons/fa6'
+
+
+//Filter Button with the DropDown Items
+const DropDown = () => {
+  return (
+    <div className="relative flex justify-center items-center border w-28 p-1 text-red-500 mr-2 rounded-md">
+        <button className="mr-1">
+          Filter
+        </button>
+        <FaAngleDown />
+      </div>
+  )
+}
+
+export default DropDown
--- src/components/FilterBtns.js
+++ src/components/FilterBtns.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import DropDown from './DropDown'
 
 const FilterBtns = () => {
   return (
-    <div>FilterBtns</div>
+    <div className="flex justify-between">
+        <DropDown/>
+        <DropDown/>
+    </div>
   )
 }
 
