da04c1553ef8b5a357c954abacc10285a3c3945c
vignesh <vigneshfornavy@gmail.com>
2024-07-31 12:01:43 +0530

    Implementaion of DropwDown btn

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -2,22 +2,27 @@ import React from "react";
 import { FaAngleDown } from "react-icons/fa6";
 
 //Filter Button with the DropDown Items
-const DropDown = ({ name, filterLists }) => {
+const DropDown = ({ name, filterLists, isOpen ,toggleBtn }) => {
   return (
     <div className="relative flex justify-center items-center border w-28 p-1 text-red-500 mr-2 rounded-md">
-      <button className="mr-1">{name}</button>
+      <button className="mr-1" onClick={toggleBtn}>{name}</button>
       <FaAngleDown />
-      <div className="absolute top-full left-0 mt-1 w-28 bg-white border rounded-md shadow-lg z-10">
-        <ul className="list-none p-2 m-0">
-          {filterLists.map((list, index) => {
-            return (
-              <li key={index} className="p-2 hover:bg-gray-200 cursor-pointer">
-                {list}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      {isOpen && (
+        <div className="absolute top-full left-0 mt-1 w-28 bg-white border rounded-md shadow-lg z-10">
+          <ul className="list-none p-2 m-0">
+            {filterLists.map((list, index) => {
+              return (
+                <li
+                  key={index}
+                  className="p-2 hover:bg-gray-200 cursor-pointer"
+                >
+                  {list}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/FilterBtns.js
+++ src/components/FilterBtns.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DropDown from './DropDown'
 import useUserInfo from '../utils/useUserInfo'
 
@@ -6,14 +6,28 @@ import useUserInfo from '../utils/useUserInfo'
 
 const FilterBtns = () => {
 
+  //state variable for toggle filter button
+  const [isCountry,SetIsCountry] = useState(false);
+  const [isGender,SetIsGender] = useState(false); 
+
   //Destructing the user info from the useUserInfo Custom Hook
   const [countries,genders] = useUserInfo();
 
+
+  //function which handles the toggle
+  const toggleCountryBtn = () =>{
+      SetIsCountry(!isCountry);
+  }
   
+  const toggleGenderBtn = () =>{
+    SetIsGender(!isGender);
+}
+
+
   return (
     <div className="flex justify-between">
-        <DropDown name={"Country"} filterLists={countries}/>
-        <DropDown name={"Gender" } filterLists={genders}/>
+        <DropDown name={"Country"} filterLists={countries} isOpen={isCountry} toggleBtn={toggleCountryBtn}/>
+        <DropDown name={"Gender" } filterLists={genders} isOpen={isGender} toggleBtn={toggleGenderBtn} />
     </div>
   )
 }
