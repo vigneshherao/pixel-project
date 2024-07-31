01e3d3402cb8fa6d18effeb263abd878879e4f16
vignesh <vigneshfornavy@gmail.com>
2024-07-31 11:21:52 +0530

    DropDown Props

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -3,11 +3,11 @@ import { FaAngleDown } from 'react-icons/fa6'
 
 
 //Filter Button with the DropDown Items
-const DropDown = () => {
+const DropDown = ({name}) => {
   return (
     <div className="relative flex justify-center items-center border w-28 p-1 text-red-500 mr-2 rounded-md">
         <button className="mr-1">
-          Filter
+          {name}
         </button>
         <FaAngleDown />
       </div>
--- src/components/FilterBtns.js
+++ src/components/FilterBtns.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import DropDown from './DropDown'
 
+
+
 const FilterBtns = () => {
   return (
     <div className="flex justify-between">
-        <DropDown/>
-        <DropDown/>
+        <DropDown name={"Country"}/>
+        <DropDown name={"Gender"}/>
     </div>
   )
 }
